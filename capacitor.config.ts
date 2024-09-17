f49e671caf655b8eda2e67d58930183263f2e849
joemafu <joe_mafu@hotmail.com>
2024-09-17 01:37:37 -0300

    Primer commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'juego.de.memoria',
   appName: 'JuegoDeMemoria',
   webDir: 'www'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB8NzA3lv-V7awfw_BzX-4CnBPBgZs2-vc",
+  authDomain: "juegodememoria-b860f.firebaseapp.com",
+  projectId: "juegodememoria-b860f",
+  storageBucket: "juegodememoria-b860f.appspot.com",
+  messagingSenderId: "315369126640",
+  appId: "1:315369126640:web:0ddb3985d717d6cb226035"
+};
